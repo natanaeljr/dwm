10afe1f2c01b2220f1f23aad2f7e9c781f2d598c
Natanael Rabello <natanael.rabello@outlook.com>
2020-05-31 19:36:47 -0300

    Add patch: centeredmaster
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- config.h
+++ config.h
@@ -52,6 +52,8 @@ static const Layout layouts[] = {
 	{ "[\\]",     dwindle }, /* fibonacci patch */
 	{ "TTT",      bstack },      /* bottomstach patch */
 	{ "===",      bstackhoriz }, /* bottomstach patch */
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -91,6 +93,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[5]} },
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[6]} },
+	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[7]} },
+	{ MODKEY|ShiftMask,             XK_c,      setlayout,      {.v = &layouts[8]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
