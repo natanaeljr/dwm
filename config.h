85391652f2b68f331a12cf968821eafee1809e73
Natanael Rabello <natanael.rabello@outlook.com>
2021-02-16 10:12:44 -0300

    Add floating terminal capability
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- config.h
+++ config.h
@@ -39,6 +39,7 @@ static const Rule rules[] = {
 	{ NULL,         NULL,       "Microsoft Teams Notification",       ~0,           1,          0,        -1 },
 	{ NULL,         NULL,       "Picture in picture",                 ~0,           1,          1,        -1 },
 	{ "Pavucontrol",NULL,       "Volume Control",                     0,            1,          1,        -1 },
+	{ "Termite",    NULL,       "Termite Floating",                   0,            1,          1,        -1 },
 };
 
 /* layout(s) */
@@ -75,11 +76,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_night, "-sf", col_gray5, NULL };
 static const char *termcmd[]  = { "termite", NULL };
+static const char *floattermcmd[]  = { "termite", "--title='Termite Floating'" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,           XK_Return, spawn,          {.v = floattermcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
