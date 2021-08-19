5c858442c6819450a2c26e11c640bb7ef971cf46
Natanael Rabello <natanael.rabello@outlook.com>
2021-08-19 13:21:02 -0300

    Support Emojis on status bar
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int rmaster            = 0;        /* 1 means master-area is initially on the right */
-static const char *fonts[]          = { "JetBrains Mono:size=10", "SF Mono:size=10", "monospace:size=10" };
-static const char dmenufont[]       = "JetBrains Mono:size=10";
+static const char *fonts[]          = { "JetBrains Mono NL:size=10", "SF Mono:size=10", "Noto Color Emoji:size=10", "monospace:size=10" };
+static const char dmenufont[]       = "JetBrains Mono NL:size=10";
 // Gray color palette
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
