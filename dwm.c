f11df5e0a2fe566cb15450fec0fe131b63cb030b
Natanael Rabello <natanael.rabello@outlook.com>
2021-09-15 18:11:10 -0300

    Center floating windows (force)
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- dwm.c
+++ dwm.c
@@ -1304,6 +1304,10 @@ manage(Window w, XWindowAttributes *wa)
 	updatewindowtype(c);
 	updatesizehints(c);
 	updatewmhints(c);
+  // { center window
+	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+	c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
+  // } center window
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
 	if (!c->isfloating)
