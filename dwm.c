c472797044647e667cfdb88e072f8264d9be32d4
Natanael Rabello <natanael.rabello@outlook.com>
2024-01-13 16:25:54 -0300

    apply patch: sizehints
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- dwm.c
+++ dwm.c
@@ -2981,7 +2981,7 @@ updatesizehints(Client *c)
 
 	if (!XGetWMNormalHints(dpy, c->win, &size, &msize))
 		/* size is uninitialized, ensure that size.flags aren't used */
-		size.flags = PSize;
+		size.flags = 0;
 	if (size.flags & PBaseSize) {
 		c->basew = size.base_width;
 		c->baseh = size.base_height;
@@ -3013,6 +3013,11 @@ updatesizehints(Client *c)
 		c->maxa = (float)size.max_aspect.x / size.max_aspect.y;
 	} else
 		c->maxa = c->mina = 0.0;
+	if(size.flags & PSize) {
+		c->basew = size.base_width;
+		c->baseh = size.base_height;
+		c->isfloating = True;
+	}
 	c->isfixed = (c->maxw && c->maxh && c->maxw == c->minw && c->maxh == c->minh);
 }
 
